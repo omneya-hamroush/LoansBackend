22fc222514f316f9f3412643593281e5f907e0a0
omneya <omneyahamroush1@gmail.com>
2022-01-05 04:14:53 +0200

    settings

--- BankLoans/settings.py
+++ BankLoans/settings.py
@@ -147,5 +147,5 @@ CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ORIGIN_WHITELIST = (
 #   'http://localhost:8080',
 # )
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
